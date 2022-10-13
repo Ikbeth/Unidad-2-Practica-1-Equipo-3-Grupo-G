3013d783676479290391fffde1987f5d4c215c6f
Ikbeth <liz.latofski@gmail.com>
2022-10-13 14:07:55 -0500

    Upload 1

---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    float valX, valZ;
+    public float velocidad = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        valX = Input.GetAxis("Horizontal");
+        valZ = Input.GetAxis("Vertical");
+        //Debug.Log("Val X: " + valX + " -- Val Z: " + valZ);
+
+        valX = valX * velocidad * Time.deltaTime;
+        valZ = valZ * velocidad * Time.deltaTime;
+
+        Vector3 v = new Vector3(valX, 0, valZ);
+
+        transform.Translate(v);
+    }
+}
